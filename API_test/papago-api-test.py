4542104f7e1bf5c09b8ca1cf9e73e17fff1bf64e
chaehuiseon <smilechs34@gmail.com>
2021-10-06 04:06:29 +0900

    translatedText 부분만 출력

--- API_test/papago-api-test.py
+++ API_test/papago-api-test.py
@@ -5,6 +5,7 @@ client_id = "" # 개발자센터에서 발급받은 Client ID 값
 client_secret = "" # 개발자센터에서 발급받은 Client Secret 값
 t = ""#번역할 내용
 encText = urllib.parse.quote(t)
+#영어 -> 한국어 번역
 data = "source=en&target=ko&text=" + encText
 url = "https://openapi.naver.com/v1/papago/n2mt"
 request = urllib.request.Request(url)
@@ -14,7 +15,13 @@ response = urllib.request.urlopen(request, data=data.encode("utf-8"))
 rescode = response.getcode()
 if(rescode==200):
     response_body = response.read()
-    print(response_body.decode('utf-8'))
+    # response_body -> byte string : decode to utf-8
+    api_callResult =response_body.decode('utf-8')
+    # JSON data will be printed as string type. So need to make it back to type JSON(like dictionary)
+    api_callResult = json.loads(api_callResult)
+    # Final Result
+    translatedText = api_callResult['message']['result']["translatedText"]
+    print(translatedText)
 else:
     print("Error Code:" + rescode)
     
